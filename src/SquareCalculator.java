e58bd0b3e239f0d77d26718c1ff684a91ce7d538
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2024-12-09 14:05:37 +0500

    java program

---
+++ src/SquareCalculator.java
@@ -0,0 +1,10 @@
+// src/SquareCalculator.java
+public class SquareCalculator {
+    public static int square(int number) {
+        return number * number;
+    }
+    public static void main(String[] args) {
+        int number = 5; // Example input
+        System.out.println("Square: " + square(number));
+    }
+}
